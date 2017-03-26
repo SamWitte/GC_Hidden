d07d07df4dd0738c129c540aa41873b1ef31bddb
Samuel Witte <witte.sam@gmail.com>
2017-03-26 15:21:04 -0700

    added tau direct spectrum

--- contour_plots.py
+++ contour_plots.py
@@ -28,35 +28,33 @@ except KeyError:
     MAIN_PATH = os.getcwd() + '/'
 
 
-def mx_sigma_contours(end_p='BB', trans='_direct_', contour_name=np.array(['_1Sigma', '_2Sigma', '_3Sigma']),
-                      gamma=1.2, scale_r=20., rfix=8.5, rho_fix=0.4):
+def mx_sigma_contours(end_p=np.array(['BB']), trans='_direct_',
+                      contour_name=np.array(['_1Sigma', '_2Sigma', '_3Sigma']),
+                      gamma=1.2, scale_r=20., rfix=8.5, rho_fix=0.4,
+                      colorl=np.array(['blue', 'red'])):
 
-    filename = MAIN_PATH + '/Test_Plots/' + end_p + trans + 'mx'
-    filename += 'Gamma_{:.2f}_ScaleR_{:.2f}_Rfix_{:.2f}_RhoFix_{:.2f}'.format(gamma, scale_r, rfix, rho_fix)
-    filename += '.pdf'
+    filename = MAIN_PATH + '/Test_Plots/'
 
     pl.figure()
     ax = pl.gca()
 
-    linestyle = ['solid', 'dashed', 'dotted']
-    colorl = ['blue', 'red']
-    for i,cc in enumerate(contour_name):
-        foi = MAIN_PATH + '/FileHolding/Contours/' + end_p + trans + 'mx_' + cc
-        foi += 'Gamma_{:.2f}_ScaleR_{:.2f}_Rfix_{:.2f}_RhoFix_{:.2f}'.format(gamma, scale_r, rfix, rho_fix)
-        foi += '.dat'
+    for j, part in enumerate(end_p):
+        filename += part + '_'
+        for i,cc in enumerate(contour_name):
+            foi = MAIN_PATH + '/FileHolding/Contours/' + part + trans + 'mx_' + cc
+            foi += 'Gamma_{:.2f}_ScaleR_{:.2f}_Rfix_{:.2f}_RhoFix_{:.2f}'.format(gamma, scale_r, rfix, rho_fix)
+            foi += '.dat'
 
-        loadf = np.loadtxt(foi)
-        loadf = loadf[loadf[:, 1] != 1.]
+            loadf = np.loadtxt(foi)
+            loadf = loadf[loadf[:, 1] != 1.]
 
-        low = interp1d(np.log10(loadf[:, 0]), np.log10(loadf[:, 1]), kind='cubic', bounds_error=False)
-        high = interp1d(np.log10(loadf[:, 0]), np.log10(loadf[:, 2]), kind='cubic', bounds_error=False)
-        massr = np.linspace(np.min(loadf[:, 0]), np.max(loadf[:, 0]), 200)
+            low = interp1d(np.log10(loadf[:, 0]), np.log10(loadf[:, 1]), kind='cubic', bounds_error=False)
+            high = interp1d(np.log10(loadf[:, 0]), np.log10(loadf[:, 2]), kind='cubic', bounds_error=False)
+            massr = np.linspace(np.min(loadf[:, 0]), np.max(loadf[:, 0]), 200)
 
-        ax.fill_between(massr, 10.**low(np.log10(massr)), 10.**high(np.log10(massr)),
-                        where=10.**high(np.log10(massr))>= 10.**low(np.log10(massr)),
-                        facecolor='blue', interpolate=True, alpha=0.2)
-        #pl.plot(massr, 10.**low(np.log10(massr)), 'r', lw=1, ls=linestyle[i])
-        #pl.plot(massr, 10.**high(np.log10(massr)), 'r', lw=1, ls=linestyle[i])
+            ax.fill_between(massr, 10.**low(np.log10(massr)), 10.**high(np.log10(massr)),
+                            where=10.**high(np.log10(massr)) >= 10.**low(np.log10(massr)),
+                            facecolor=colorl[j], interpolate=True, alpha=0.2)
 
     ax.set_xlabel(r'$m_\chi$  [GeV]', fontsize=fs)
     ax.set_ylabel(r'$<\sigma {\rm v}>$  [$cm^{3} s^{-1}$]', fontsize=fs)
@@ -65,6 +63,10 @@ def mx_sigma_contours(end_p='BB', trans='_direct_', contour_name=np.array(['_1Si
     plt.ylim(ymin=1.*10**-27., ymax=1.*10**-25.)
     ax.set_xscale("log")
     ax.set_yscale("log")
+
+    filename += trans + 'mx'
+    filename += 'Gamma_{:.2f}_ScaleR_{:.2f}_Rfix_{:.2f}_RhoFix_{:.2f}'.format(gamma, scale_r, rfix, rho_fix)
+    filename += '.pdf'
 
     plt.savefig(filename)
 
